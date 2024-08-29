4e201a8583c852bd146e7e1afd31240d30b59e18
MaurZac <maur.mx@gmail.com>
2024-08-29 14:12:28 -0600

    Starting binding on activity list.

--- app/src/main/java/com/example/superherolist/MainActivity.kt
+++ app/src/main/java/com/example/superherolist/MainActivity.kt
@@ -3,12 +3,36 @@ package com.example.superherolist
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.superherolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+
+    }
+
+    private fun initUI() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searByName(query.orEmpty())
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?) = false
+
+
+        })
+    }
+
+    private fun searByName(query: String?) {
+
     }
 }
